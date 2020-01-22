66d6bfcaada87496b0ce86b2e145a60d6c9ffa5c
Syed Umair <umairrsyedd@gmail.com>
2020-01-22 05:40:14 +0530

    API is working perfectly and custom inputs are also working, however if the user is giving an invalid response for the movie name, it is throwing an error. Styling of the page is also yet to be completed

--- app.js
+++ app.js
@@ -1,7 +1,30 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
+const request= require('request')
+//--------------------------------------------------------------------------------------------
+app.set('view engine',"ejs")
+//--------------------------------------------------------------------------------------------
+app.get("/",function(req,res){
+    console.log("Someone Requested For The Home Page")
+    res.render('index')
+})
+app.get("/results",function(req,res){
+    console.log("Someone Requested For The Movies Page")
+    var query = req.query.search
+    var url = "http://www.omdbapi.com/?s="+query+"&apikey=thewdb"
+    request(url, function(error,response,body){
+    if(!error && response.statusCode==200)
+    {
+        var data = JSON.parse(body)
+        //res.send(results["Search"][0])
+        res.render('results',{data : data})
+    }
+    })
+})
 
 
-app.listen(2000,()=>{
+
+
+app.listen(3000,function(){
     console.log("Server Has Started Successfully")
-}) 
+});
